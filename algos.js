8e4979612d10a4fbe9866575ca1e1f1108ef872b
David Mackey <davidmackey92@gmail.com>
2022-11-22 19:21:11 -0600

    Solved EvenOrOdd Kata

---
+++ algos.js
@@ -0,0 +1,21 @@
+//Codewars
+//EvenOrOdd Kata
+
+// Solution:
+// function evenOrOdd(number) {
+//   number = number.toString()
+//   number = number[number.length - 1]
+//   console.log(number)
+//   if (number == 2 || number == 4 || number == 6 || number == 8) {
+//     return `Even`
+//   } else return 'Odd'
+// }
+
+// Mathematical Solution:
+// function evenOrOdd(number) {
+//   if (number % 2 == 0) {
+//     return "Even"
+//   } return "Odd"
+// }
+
+// console.log(evenOrOdd(123456789))
