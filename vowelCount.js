769341a99c9c1a13f73af99a7a221808bee1b433
David Mackey <davidmackey92@gmail.com>
2023-01-11 21:36:46 -0600

    vowelsCount

---
+++ vowelCount.js
@@ -0,0 +1,16 @@
+// vowelCount
+
+function getCount(str) {
+  return (str.match(/[aeiou]/ig) || []).length;
+}
+
+// function getCount(str) {
+//   let vowelsCount = 0
+//   const vowels = ['a', 'e', 'i', 'o', 'u']
+//   for (let char of str) {
+//     if (vowels.includes(char)) {
+//       vowelsCount++
+//     }
+//   }
+//   return vowelsCount
+// }
